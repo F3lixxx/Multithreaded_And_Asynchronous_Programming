1d40cb6fb49da03e7c3c3556af1ad81785ebd658
“yenox” <f3lixxx@yandex.ru>
2024-02-21 13:55:46 +0300

    Change code in HW1_v6

--- Multithreaded/HW1/main.cpp
+++ Multithreaded/HW1/main.cpp
@@ -8,24 +8,21 @@ std::once_flag flag;
 std::once_flag flag2;
 
 
-void queue_func(int &clients) {
-    for (int i = 0; i <= clients; ++i) {
-        std::this_thread::sleep_for(1000ms);
-        std::call_once(flag, []() {
-            std::cout << "Clients ";
-            });
-        std::cout << i << '\t';
-    }
-    std::cout << std::endl;
+void queue_func(int& clients) {
+        int f = 0;
+        while (f != clients)
+        {
+            std::this_thread::sleep_for(1000ms);
+            f++;
+            std::cout << " Clients " << f << ' ' << std::endl;
+        }
 }
-void teller_func(int &clients) {
 
-    for (int i = clients; i >= 0; --i) {
+void teller_func(int& clients) {
+        while(clients != 0){
         std::this_thread::sleep_for(2000ms);
-        std::call_once(flag2, []() {
-            std::cout << "Tellers ";
-            });
-        std::cout << i << '\t';
+            clients--;
+            std::cout << " Tellers " << clients << ' ' << std::endl;
     }
 }
 
@@ -33,15 +30,11 @@ int main() {
     int k = 10;
     std::cout << "Your proccessor have " << std::thread::hardware_concurrency() << " cores" << std::endl;
 
-    for (int i = 0; i < 1; ++i) {
         std::thread queue(queue_func, std::ref(k));
-        queue.join();
-
-    }
-    for (int i = 0; i < 1; ++i) {
         std::thread teller(teller_func, std::ref(k));
+
+        queue.join();
         teller.join();
-    }
 
     return 0;
 }
