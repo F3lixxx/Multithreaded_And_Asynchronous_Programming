cf7ef4b4b42baaf17fc61fe9780dfcf5c23ed9de
“yenox” <f3lixxx@yandex.ru>
2024-02-22 12:25:27 +0300

    Change code in HW1_v7

--- Multithreaded/HW1/main.cpp
+++ Multithreaded/HW1/main.cpp
@@ -9,20 +9,19 @@ std::once_flag flag2;
 
 
 void queue_func(int& clients) {
-        int f = 0;
-        while (f != clients)
-        {
-            std::this_thread::sleep_for(1000ms);
-            f++;
-            std::cout << " Clients " << f << ' ' << std::endl;
-        }
+    for (int i = 0; i < clients; i++)
+    {
+        std::this_thread::sleep_for(1000ms);
+        clients++;
+        std::cout << " Clients " << i << ' ' << std::endl;
+    }
 }
 
 void teller_func(int& clients) {
-        while(clients != 0){
+    while (clients != 0) {
         std::this_thread::sleep_for(2000ms);
-            clients--;
-            std::cout << " Tellers " << clients << ' ' << std::endl;
+        --clients;
+        std::cout << " Tellers " << clients << ' ' << std::endl;
     }
 }
 
@@ -30,11 +29,11 @@ int main() {
     int k = 10;
     std::cout << "Your proccessor have " << std::thread::hardware_concurrency() << " cores" << std::endl;
 
-        std::thread queue(queue_func, std::ref(k));
-        std::thread teller(teller_func, std::ref(k));
+    std::thread queue(queue_func, std::ref(k));
+    std::thread teller(teller_func, std::ref(k));
 
-        queue.join();
-        teller.join();
+    queue.join();
+    teller.join();
 
     return 0;
 }
