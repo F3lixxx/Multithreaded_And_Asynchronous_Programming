406270bb40b2f960c94cdaeb93d9bd93150f481c
“yenox” <f3lixxx@yandex.ru>
2024-02-22 18:40:21 +0300

    Change code in HW2_v2

--- Multithreaded/HW2/main.cpp
+++ Multithreaded/HW2/main.cpp
@@ -11,28 +11,27 @@ void calculate_sum(std::vector<int>& vec1, std::vector<int>& vec2, std::vector<i
 }
 
 int main() {
-    std::cout << "Available hardware cores: " << std::thread::hardware_concurrency() << std::endl;
+    std::cout << std::setw(20) << "1000" << std::setw(20) << "10000" << std::setw(20) << "100000" << std::setw(17) << "1000000" << std::endl;
 
-    std::vector<int> sizes = { 1000, 10000, 100000, 1000000 };
-    std::vector<int> num_threads = {1, 2, 4, 8, 16 };
-    for (auto size : sizes)
-    {
-        std::vector<int> vec1(size, 10000);
-        std::vector<int> vec2(size, 10000);
-        std::vector<int> result(size);
+    std::vector<int> num_threads = { 1, 2, 4, 8, 16 };
 
-        std::cout << size << std::endl;
+    for (auto thread : num_threads) {
+        std::cout << thread << " cores ";
 
-        
+        std::vector<int> sizes = { 1000, 10000, 100000, 1000000 };
 
-        for (auto threads : num_threads) {
+        for (auto size : sizes) {
+            std::vector<int> vec1(size, 10000);
+            std::vector<int> vec2(size, 10000);
+            std::vector<int> result(size);
+
             std::vector<std::thread> thread_pool;
 
             auto start_time = std::chrono::high_resolution_clock::now();
 
-            for (int i = 0; i < threads; ++i) {
-                int start = i * (size / threads);
-                int end = (i == threads - 1) ? size : (i + 1) * (size / threads);
+            for (int i = 0; i < thread; ++i) {
+                int start = i * (size / thread);
+                int end = (i == thread - 1) ? size : (i + 1) * (size / thread);
                 thread_pool.emplace_back(calculate_sum, std::ref(vec1), std::ref(vec2), std::ref(result), start, end);
             }
 
@@ -41,13 +40,13 @@ int main() {
             }
 
             auto end_time = std::chrono::high_resolution_clock::now();
-            auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time);
+            auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
 
-            std::cout << "Threads: " << threads << ", Time: " << duration.count() / 1000.0 << " ms" << std::endl;
+            std::cout << std::setw(15) << std::fixed << std::setprecision(7) << duration.count() / 1000.0 << " ms";
         }
+
         std::cout << std::endl;
-        }
-   
+    }
 
     return 0;
 }
